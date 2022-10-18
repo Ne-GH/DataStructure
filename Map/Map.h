b97fb2edc3757f0470ee3c9b3726e7bb50d8f68f
Ne-gh <printfne@gmail.com>
2022-10-18 14:01:16 +0800

    add new structure Map

---
+++ Map/Map.h
@@ -0,0 +1,23 @@
+/*********************************************************************
+ * Auther : yongheng
+ * Date : 2022/10/18 13:59
+*********************************************************************/
+#ifndef DATASTRUCTURE_MAP_H
+#define DATASTRUCTURE_MAP_H
+
+template<typename K,typename V>
+class Map{
+private:
+    K _key;
+    V _val;
+public:
+
+    Map(){
+
+    }
+    ~Map(){
+
+    }
+
+};
+#endif //DATASTRUCTURE_MAP_H

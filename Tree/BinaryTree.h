655bf0c6938615bc5fdc48fb0fdb231def6c8ade
Ne-gh <printfne@gmail.com>
2022-10-19 15:48:20 +0800

    add BinaryTree.h in Tree

---
+++ Tree/BinaryTree.h
@@ -0,0 +1,14 @@
+/*********************************************************************
+ * Auther : yongheng
+ * Date : 2022/10/19 15:46
+*********************************************************************/
+#ifndef DATASTRUCTURE_BINARYTREE_H
+#define DATASTRUCTURE_BINARYTREE_H
+
+template<typename T>
+class BinaryTree{
+
+};
+
+
+#endif //DATASTRUCTURE_BINARYTREE_H

f426d57ef5a039fc9f8e473541ebeb0cb66bfaae
Ne-gh <printfne@gmail.com>
2022-10-19 20:57:06 +0800

    add some function in BinaryTree.h, Init,Insert Destory

--- Tree/BinaryTree.h
+++ Tree/BinaryTree.h
@@ -4,23 +4,154 @@
 *********************************************************************/
 #ifndef DATASTRUCTURE_BINARYTREE_H
 #define DATASTRUCTURE_BINARYTREE_H
+#include <iostream>
 
 template<typename T>
-class BinaryTree{
-    class _Node{
-        T _val;
-        BinaryTree *_left;
-        BinaryTree *_right;
-    };
+class TreeNode{
 public:
-    BinaryTree(){
-
+    T _val;
+    TreeNode *_left;
+    TreeNode *_right;
+    TreeNode(){  }
+    TreeNode(T val){
+        _val = val;
     }
-    ~BinaryTree(){
-
+    friend std::ostream & operator << (std::ostream &out,TreeNode node){
+        out << node._val;
+        return out;
     }
-
+    friend std::ostream & operator << (std::ostream &out,TreeNode *pnode){
+        out << pnode->_val;
+        return out;
+    }
 };
 
 
+
+template<typename T>
+class BinaryTree{
+
+    TreeNode<T> *_root;
+public:
+    BinaryTree(){
+        _root = new TreeNode<T>();
+        _root->_left = _root->_right = nullptr;
+    }
+    BinaryTree(T data){
+        new (this)BinaryTree();
+        _root->_val = data;
+    }
+
+
+    void Insert(T data){
+
+        _Insert(_root,data);
+
+
+    }
+    void PreOrderTraversal(){
+        _PreOrderTraversal(_root);
+    }
+    void LnOrderTraversal(){
+        _LnOrderTraversal(_root);
+    }
+    void PostOrderTraversal(){
+        _PostOrderTraversal(_root);
+    }
+    void Destroy(){
+        _Destroy(_root);
+        _root = nullptr;
+    }
+    ~BinaryTree(){
+        Destroy();
+    }
+
+
+
+    TreeNode<T> *Root(){
+        return _root;
+    }
+
+    friend std::ostream & operator << (std::ostream &out,BinaryTree * bt){
+        out << bt->_val;
+        return out;
+    }
+
+};
+template<typename T>
+void _PreOrderTraversal(TreeNode<T> *root){
+    if(root == nullptr)
+        return;
+    std::cout << root->_val << " ";
+    _PreOrderTraversal(root->_left);
+    _PreOrderTraversal(root->_right);
+}
+
+template<typename T>
+void _LnOrderTraversal(TreeNode<T> *root){
+    if(root == nullptr)
+        return;
+
+    _LnOrderTraversal(root->_left);
+    std::cout << root->_val << " ";
+    _LnOrderTraversal(root->_right);
+
+}
+template<typename T>
+void _PostOrderTraversal(TreeNode<T> *root){
+    if(root == nullptr)
+        return;
+    _PostOrderTraversal(root->_left);
+    _PostOrderTraversal(root->_right);
+    std::cout << root->_val << " ";
+}
+
+/* 带头节点一起销毁 */
+template<typename T>
+void _Destroy(TreeNode<T> *root){
+
+    if(root != nullptr){
+        if(root->_left != nullptr){
+            _Destroy(root->_left);
+        }
+        if(root->_right != nullptr){
+            _Destroy(root->_right);
+        }
+
+        delete root;
+    }
+
+    else{
+        return;
+    }
+    return;
+}
+template<typename T>
+void _Insert(TreeNode<T> *root,T data){
+    if(root == nullptr)
+        return;
+
+    if(root->_val > data){
+        if(root->_left == nullptr){
+            TreeNode<T> *node = new TreeNode<T>(data);
+            root->_left = node;
+            node->_left = node->_right = nullptr;
+        }
+        else{
+            _Insert(root->_left,data);
+        }
+    }
+        /* root->val <= data */
+    else{
+        if(root->_right == nullptr){
+            TreeNode<T> *node = new TreeNode<T>(data);
+            root->_right = node;
+            node->_left = node->_right = nullptr;
+        }
+        else{
+            _Insert(root->_right,data);
+        }
+    }
+
+}
 #endif //DATASTRUCTURE_BINARYTREE_H
--- demo.cpp
+++ demo.cpp
@@ -7,7 +7,7 @@
 #include "List/List.h"
 #include "Stack/Stack.h"
 #include "Queue/Queue.h"
-
+#include "Tree/BinaryTree.h"
 #include <iostream>
 
 using std::cin,std::cout,std::endl;
@@ -15,7 +15,20 @@ using std::cin,std::cout,std::endl;
 
 int main(int argc,char *argv[]){
 
+    BinaryTree<int> bt(10);
+    bt.Insert(5);
+    bt.Insert(11);
+    bt.Insert(10);
+    bt.Insert(1);
+    bt.Insert(9);
 
+    bt.PreOrderTraversal();
+    cout << endl;
+    bt.LnOrderTraversal();
+    cout << endl;
+    bt.PostOrderTraversal();
+    cout << endl;
+    bt.Destroy();
 
     return 0;
 }
